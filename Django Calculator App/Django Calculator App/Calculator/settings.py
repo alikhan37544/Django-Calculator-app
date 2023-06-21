3f8918bc8c2b5f323b19a05aa4f34bedd8096c61
Ali Khan <alikhan37544@gmail.com>
2023-06-21 09:56:37 +0530

    Making tons of changes

--- Django Calculator App/Django Calculator App/Calculator/settings.py
+++ Django Calculator App/Django Calculator App/Calculator/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Calculator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'calc_log',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',  # Usually 'localhost' or '127.0.0.1'
+        'PORT': '',  # Leave it as an empty string for the default port
     }
 }
 
@@ -117,9 +121,4 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# https
